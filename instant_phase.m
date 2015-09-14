423008dc0058181d2f57a31f983b10f1d3e5005d
William Marshall <wmarshall3@wisc.edu>
2015-09-13 23:35:14 -0500

    comments

--- instant_phase.m
+++ instant_phase.m
@@ -1,21 +1,29 @@
 function P = instant_phase(x, sRate, centre, width)
+% Estimate the instantaneous phase of a time series in frequency band using
+% the Hilbert transform.
+%
+% Args: 
+%   x (array): time series to be analyzed
+%   sRate (int): Sampling rate of the signal
+%   centre (float): Centre of the frequency band of interest
+%   width (float): Width of the band of interest (must be symmetric)
+% 
+% Returns:
+%   P (array): Estimated instantaneous phase of the signal.
 
-% Estimate the instantaneous phase of a time series x
-% In the band centred at freq with length 2*width
-
-% Phase is not unwrapped, and no Burn period is remove
-
+% Coerce x to a double
 x = double(x);
- 
+
+% Transform frequency component to zero
 t = 0:(length(x)-1);
-
-[b,a]=butter(4, 2 * width / sRate, 'low');
-
 Y1 = x .* sin(2 * pi * centre / sRate * t);
 Y2 = x .* cos(2 * pi * centre / sRate * t);
 
+% Low-pass filter power outside band of interest
+[b,a]=butter(4, 2 * width / sRate, 'low');
 F1 = filtfilt(b, a, Y1);        
 F2 = filtfilt(b, a, Y2);    
 
+% Solve for phase
 P = atan2(F2, F1);
 
