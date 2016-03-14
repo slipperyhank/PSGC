0ceb687e8df15df983234de462ef28baa8f8c412
William Marshall <wmarshall3@wisc.edu>
2016-03-14 17:35:03 -0500

    Remove bin_time parameter, adjust default IC

--- PSGC.m
+++ PSGC.m
@@ -1,4 +1,4 @@
-function [likelihood, final_parameters, indices]=PSGC(points, history, bin_time, varargin)
+function [likelihood, final_parameters, indices]=PSGC(points, history, varargin)
 % Numerical solve for the maximum likelihood estimates of a PSGC model 
 % using Newtons method
 %
@@ -29,28 +29,34 @@ initial_history = history;
 n_initial_parameters = size(history, 2);
 
 % Optional initial conditions
-if nargin < 4
-    initial_conditions = zeros(n_initial_parameters, 1);
+if nargin < 3
+    initial_conditions = zeros(n_initial_parameters, 1) - 1;
+    step_size = 0.2;
+elseif nargin == 3
+    initial_conditions = varargin{1};
     step_size = 0.2;
 elseif nargin == 4
     initial_conditions = varargin{1};
-    step_size = 0.2;
-elseif nargin == 5
-    initial_conditions = varargin{1};
     step_size = varargin{2};
 else
-    error('Wrong number of inputs, must be three or four')
+    error('Must be at least 2 inputs. Option inputs are initial conditions and step size')
 end
 
-% Input points should be a column vector. If it is a row, transpose
+% Input points and initial conditions should be a column vector. 
+% If it is a row, transpose
 if isrow(points)
     points = points';
 end
-% Validate that points is now a column
+if isrow(initial_conditions)
+    initial_conditions = initial_conditions';
+end
+% Validate that they are now columns
 if ~iscolumn(points)
     error('points input must be a column vector')
 end
-
+if ~iscolumn(initial_conditions)
+    error('initial conditions must be a column vector')
+end
 
 % First remove all parameters that are trivially -Inf
 % i.e., if this event has occured, then the current process will
@@ -91,9 +97,9 @@ indices = {inhibitory_indices, excitatory_indices, estimated_indices};
 flag=0;
 % convergence criteria
 epsilon=0.00001;
-% number of segments
+min_step_size = 0.01;
+% number of segments and parameters to estimate
 n_bins = length(points);
-% number of parameters to estimate
 n_parameters = size(history, 2);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -119,21 +125,21 @@ while 1 == 1
         % Calculate CIF
         lambda(bin) = exp(sum(parameters' .* history(bin, :)));
         % If probability of a shift grows too large, model fails
-        if lambda(bin) * bin_time > 1
+        % Initial conditions should be less than 0 to accomodate
+        if lambda(bin) > 1
             flag=1;
         end
     end
     if flag==1
         break
     end
-    
     % Calculate gradiant (F) and Jacobian (J) for newtons method
     for parameter1 = 1:n_parameters
-        F(parameter1) = -sum(points .* history(:, parameter1) + history(:, parameter1) .* (bin_time * lambda .* ...
-            (points - 1)) ./ (1 - bin_time .* lambda));
+        F(parameter1) = -sum(points .* history(:, parameter1) + history(:, parameter1) .* (lambda .* ...
+            (points - 1)) ./ (1 - lambda));
         for parameter2 = 1:n_parameters
             J(parameter1, parameter2) = sum(history(:, parameter1) .* history(:, parameter2) .* (points - 1) .* ...
-                bin_time .* lambda ./ (1 - lambda .* bin_time) .^ 2);
+                lambda ./ (1 - lambda) .^ 2);
         end
     end
     % Calculate change in parameters. Magnitude is reduced to 20% to
@@ -141,7 +147,8 @@ while 1 == 1
     % of the model
     change_in_parameters = J \ F;
     parameters = parameters + change_in_parameters * step_size; 
-    % If any of the parameters are below -20, set flag 2 and break
+    % If any of the parameters are below -20, set flag 2
+    % Step size too large, or inhibitory parameter
     if min(parameters) < -10
         flag = 2;
         break;
@@ -157,14 +164,15 @@ if flag == 0
     for bin = 1:n_bins
         lambda(bin) = exp(sum(parameters' .* history(bin,:))); 
     end
-    likelihood = sum((points .* log(lambda .* bin_time) + (1 - points) .* log(1 - lambda .* bin_time)));
+    likelihood = sum((points .* log(lambda) + (1 - points) .* log(1 - lambda)));
     final_parameters = zeros(1, n_initial_parameters);
     final_parameters(estimated_indices) = parameters;
     final_parameters(inhibitory_indices) = -Inf;
     final_parameters(excitatory_indices) = Inf;
-elseif step_size > 0.01
+elseif step_size > min_step_size
+    % flag set - try with smaller step size until below minimum
     flag
-    [likelihood, final_parameters, indices]=PSGC(initial_points, initial_history, bin_time, initial_conditions, step_size / 2);
+    [likelihood, final_parameters, indices] = PSGC(initial_points, initial_history, initial_conditions, step_size / 2);
 else
     error('Error: Model does not converge. Possible inhibitory or excitatory parameter')
 end
