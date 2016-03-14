3438565c2bf194603de2261bdc763cff3f21df58
William Marshall <wmarshall3@wisc.edu>
2016-03-14 15:38:38 -0500

    psgc preprocess inhibitory and excitatory parameters

--- PSGC.m
+++ PSGC.m
@@ -1,89 +1,153 @@
-function [L,Gamma]=PSGC(dN,R,x0)
+function [likelihood, parameters]=PSGC(points, history, bin_time, varargin)
+% Numerical solve for the maximum likelihood estimates of a PSGC model 
+% using Newtons method
+%
+% Args: 
+%   points (array): The binned phase shift events a single channel
+%   history (array): The history values of each channel
+%   bin_time (float): The time in seconds of each bin
+%   initial (array(float)): Option intial conditions for optimization
+%
+% Returns:
+%   L (float): Maximum likelihood value
+%   Gamma (array(float)): Maximum likelihood estimates
 
+% Input points should be a column vector. If it is a row, transpose
+if isrow(points)
+    points = points';
+end
+% Validate that points is now a column
+if ~iscolumn(points)
+    error('points input must be a column vector')
+end
 
-global delta
+% First remove all parameters that are trivially -Inf
+% i.e., if this event has occured, then the current process will
+% not have an event
+inhibitory_parameters = find(points'*history == 0);
+[inhibitory_bins, ~] = find(history(:, inhibitory_parameters) == 1);
 
+history(:, inhibitory_parameters) = [];
+history(inhibitory_bins, :) = [];
+points(inhibitory_bins) = [];
+
+% Now remove all parameters taht are trivially +Inf
+% i.e., if this event has occured, the the current process will
+% definitely have an event.
+
+excitatory_parameters = find(points'*history == sum(history));
+[excitatory_bins, ~] = find(history(:, excitatory_parameters) == 1);
+
+history(:, excitatory_parameters) = [];
+history(excitatory_bins, :) = [];
+points(excitatory_bins) = [];
+
+% Flag for determining recursion or model fit error or no problems
+flag=0;
 
 % convergence criteria
 epsilon=0.00001;
 
 % number of segments
-K=length(dN);
+n_bins = length(points);
 
 %number of parameters
-S=length(R(:)) / K;
-index=1:S;
-lambda=zeros(K,1);
-flag=0;
+n_parameters = size(history, 2);
 
-%Initial Conditions
-X=x0;
+%Initialize output
+parameters = zeros(n_parameters, 1);
+likelihood = 0;
 
-if sum(X<=-10) == 0
-    
-    Bad=X;
-    index=1:S;
-    %initialize Jacobian (J) and Derivative (F)
-    J=zeros(S);
-    F=zeros(S,1);
-
-    while 1==1
-        for k=1:K
-            lambda(k)=exp(sum(X'.*R(k,:))); 
-            if lambda(k)*delta > 1
-                flag=1;
-                break
-            end
-        end
-        if flag==1
-            break
-        end
-    
-        for k=1:length(X)
-            F(k) = sum((dN.*R(:,k) + R(:,k).*(delta*lambda.*(dN-1))./(1-delta.*lambda)));
-        end
-        for k=1:length(X)
-            for j=1:length(X)
-                J(k,j) = sum((R(:,k).*R(:,j).*(dN-1).*delta.*lambda./(1-lambda.*delta).^2));
-            end
-        end
-    
-        F=-F;
-        dX = J\F;
-    
-        X = X + dX/5;
-    
-    
-        if min(X) <= -10
-            Bad=X<=-10;
-            flag=2;
-            break;
-        end
-        if max(abs(dX)) < epsilon 
-            break
-        end
-    end
+% Optional initial condition input
+% Initialize parameters (X)
+if nargin < 4
+    X = zeros(n_parameters, 1);
+elseif nargin == 4
+    X = varargin{1};
 else
-    Bad=X<=-10;
-    flag=2;
+    error('Wrong number of inputs, must be three or four')
 end
-if flag==0
-    Gamma=X;
-    for k=1:K
-        lambda(k)=exp(sum(X'.*R(k,:))); 
+
+% Index for parameters
+index = 1:n_parameters;
+
+% Bad measures parameters which go below the minimum (-10)
+%Bad = X;
+
+%initialize Jacobian (J) and Derivative (F) and intensity (lambda) for
+%optimzation method
+J = zeros(n_parameters);
+F = zeros(n_parameters, 1);
+lambda = zeros(n_bins, 1);
+
+% Optimize 
+while 1 == 1
+    for bin = 1:n_bins
+        % Calculate lambda
+        lambda(bin) = exp(sum(X' .* history(bin, :)));
+        % If probability of a shift grows too large, model fails
+        if lambda(bin) * bin_time > 1
+            flag=1
+        end
     end
-    L = sum((dN.*log(lambda.*delta) + (1-dN).*log(1-lambda.*delta)));
+    lambda * bin_time
+    if flag==1
+        error('Probability greater than 1')
+        break
+    end
+    
+    % Calculate gradiant (F) and Jacobian (J) for newtons method
+    for parameter1 = 1:n_parameters
+        F(parameter1) = -sum(points .* history(:, parameter1) + history(:, parameter1) .* (bin_time * lambda .* ...
+            (points - 1)) ./ (1 - bin_time .* lambda));
+        for parameter2 = 1:n_parameters
+            J(parameter1, parameter2) = sum(history(:, parameter1) .* history(:, parameter2) .* (points - 1) .* ...
+                bin_time .* lambda ./ (1 - lambda .* bin_time) .^ 2);
+        end
+    end
+    % Calculate change in parameters dX. Magnitude is reduced to 20% to
+    % prevent parameters from over shooting and jumping outside the support 
+    % of the model
+    F
+    J
+    dX = J \ F
+    X = X + dX / 200
+    % If any of the parameters are below -10, go to recursion step
+    if min(X) < -100
+        %Bad = X < -10;
+        flag = 2
+        break;
+    end
+    % when convergence is reached, exit loop
+    
+    if max(abs(dX)) < epsilon 
+        break
+    end
 end
 
-if flag==1
-    L=0;
-    Gamma=X*0;
+% flag=0: No errors, calculate output
+if flag == 0
+    parameters = X;
+    for bin = 1:n_bins
+        lambda(bin) = exp(sum(X' .* history(bin,:))); 
+    end
+    likelihood = sum((points .* log(lambda .* bin_time) + (1 - points) .* log(1 - lambda .* bin_time)));
 end
 
-if flag==2
-    Gamma(index(Bad==1))=-10;
-    index(Bad==1)='';
-    ROld='';
-    [L,temp]=PSGC2(dN,R,Bad,ROld,X(Bad==0));
-    Gamma(index)=temp;
+% flag=1: error, must be looked at
+if flag == 1
+    %likelihood = 0;
+    %parameters = X .* 0;
+    flag
+end
+
+% flag=2: parameters below -10, remove from optimization and called
+% function again
+if flag == 2
+    flag
+    %parameters(index(Bad==1)) = -10;
+    %index(Bad==1) = '';
+    %ROld = '';
+    %[likelihood, temp] = PSGC2(points, R, Bad, ROld, X(Bad==0));
+    %parameters(index) = temp;
 end
