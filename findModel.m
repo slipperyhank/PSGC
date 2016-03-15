98558805ef4a50c82dc7c390fc8ebae863d90d43
William Marshall <wmarshall3@wisc.edu>
2016-03-15 12:01:12 -0500

    Update findModel style

--- findModel.m
+++ findModel.m
@@ -1,34 +1,36 @@
-function [maxM, g, AIC] = findModel(dN, W, nM)
+function [model_order, best_parameters, AIC] = findModel(points, bins_per_window, max_windows)
 % Find the model order for each channel
 % Args:
-%   dN (array): The number of shifts in each bin
-%   W (int): The number of data points in each history window
-%   nM (int): The maximum number of history windows to try
+%   points (array): The number of shifts in each bin
+%   bins_per_window (int): The number of data points in each history window
+%   max_windows (int): The maximum number of history windows to try
 %
 % Returns:
-    % maxM (int): The optimal number of history windows
-    % g (array): The set of parameters for the optimal model
+    % model_order (int): The optimal number of history windows
+    % best_parameters (array): The set of parameters for the optimal model
     
 
 % Initialize model order, maximum likelihood and parameters values
-nChan = size(dN, 1);
-maxM = zeros(1, nChan);
-AIC = zeros(1, nChan);
-g = cell(1, nChan);
+n_channels = size(points, 1);
+model_order = zeros(1, n_channels);
+AIC = zeros(1, n_channels);
+best_parameters = cell(1, n_channels);
 
-for M=1:nM;
-     % Calculate H for each channel
-     H = makeHistory(dN, M, W, delta);
-     % Calculate likelihood and parameter estimates for each
-     % channel
-     for c = 1:nChan
-         [Likelihood, Gamma]=PSGC_noIC(dN(c, (M * W +1):end), H((M * W + 1):end, :));
-         % If this likelihood is better than all previous, update the AIC,
-         % maxM and g
-         if 2 * (1 + nChan * M - Likelihood) < AIC(c)
-             AIC(c) = 2 * (1 + nChan * M - Likelihood);
-             g{c} = Gamma;
-             maxM(c) = M;
-         end
-     end
+for n_windows = 1:max_windows;
+    n_windows
+    % Calculate H for each channel
+    history = makeHistory(points, bins_per_window, n_windows);
+    % Calculate likelihood and parameter estimates for each
+    % channel
+    for channel = 1:n_channels
+        channel
+        [likelihood, parameters] = fit_model(points(channel, (n_windows * bins_per_window +1):end), history((n_windows * bins_per_window + 1):end, :));
+        % If this likelihood is greater than all previous, update the AIC,
+        % model_order and best_parameters
+        if 2 * (1 + n_channels * n_windows - likelihood) < AIC(channel)
+            AIC(channel) = 2 * (1 + n_channels * n_windows - likelihood);
+            best_parameters{channel} = parameters;
+            model_order(channel) = n_windows;
+        end
+    end
 end
