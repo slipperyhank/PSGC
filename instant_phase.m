16ba0af2e75b7f296f5fe3d26f9457c6e9a63bff
William Marshall <wmarshall3@wisc.edu>
2016-03-16 15:56:35 -0500

    instant_phase code style

--- instant_phase.m
+++ instant_phase.m
@@ -1,29 +1,37 @@
-function P = instant_phase(x, sRate, centre, width)
+function [phase, magnitude] = instant_phase(signal, sampling_rate, band_centre, band_width)
 % Estimate the instantaneous phase of a time series in frequency band using
 % the Hilbert transform.
 %
 % Args: 
-%   x (array): time series to be analyzed
-%   sRate (int): Sampling rate of the signal
-%   centre (float): Centre of the frequency band of interest
-%   width (float): Width of the band of interest (must be symmetric)
+%   signal (array): time series to be analyzed
+%   sampling_rate (int): Sampling rate of the signal
+%   band_centre (float): Centre of the frequency band of interest
+%   band_width (float): Width of the band of interest (must be symmetric)
 % 
 % Returns:
-%   P (array): Estimated instantaneous phase of the signal.
+%   phase (array): Estimated instantaneous phase of the signal.
+%   magnitude (array): Estimated instantaneous phase of the signal.
+
+% TODO - make filter order an optional input
+filter_order = 4;
 
 % Coerce x to a double
-x = double(x);
+signal = double(signal);
 
 % Transform frequency component to zero
-t = 0:(length(x)-1);
-Y1 = x .* sin(2 * pi * centre / sRate * t);
-Y2 = x .* cos(2 * pi * centre / sRate * t);
+t = 0:(length(signal) - 1);
+
+in_phase = signal .* sin(2 * pi * band_centre / sampling_rate * t);
+quadrature = signal .* cos(2 * pi * band_centre / sampling_rate * t);
 
 % Low-pass filter power outside band of interest
-[b,a]=butter(4, 2 * width / sRate, 'low');
-F1 = filtfilt(b, a, Y1);        
-F2 = filtfilt(b, a, Y2);    
+[b, a] = butter(filter_order, 2 * band_width / sampling_rate, 'low');
+
+% Filter the in_phase and in_quadrature terms
+in_phase = filtfilt(b, a, in_phase);        
+quadrature = filtfilt(b, a, quadrature);    
 
 % Solve for phase
-P = atan2(F2, F1);
+phase = atan2(quadrature, in_phase);
+magnitude = sqrt(in_phase .^ 2 + quadrature .^ 2);
 
